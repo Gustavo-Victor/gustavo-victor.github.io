dd3810cb0c4c68efd54c94fdb747ac68e5550133
Gustavo Victor <gguvictor909@gmail.com>
2024-07-06 15:10:01 -0300

    feat: add about animation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import aboutImage from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../data/info";
+import { motion } from "framer-motion";
 
 
 export default function About() {
@@ -10,16 +11,27 @@ export default function About() {
                 <span className="text-neutral-500"> Mim</span>
             </h2>
             <div className="flex flex-wrap">
-                <div className="w-full lg:w-1/2 lg:p-8">
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100, }}
+                    transition={{ duration: 0.5 }}
+                    className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
-                        <img className="rounded-2xl" src={aboutImage} alt="about-image" />
+                        <img
+                            className="rounded-2xl"
+                            src={aboutImage}
+                            alt="about-image" />
                     </div>
-                </div>
-                <div className="w-full lg:w-1/2">
+                </motion.div>
+                <motion.div
+                    whileInView={{x: 0, opacity: 1}}
+                    initial={{x: 100, opacity: 0}}
+                    transition={{duration: 0.5}} 
+                    className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:justify-start">
                         <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </section>
     )
