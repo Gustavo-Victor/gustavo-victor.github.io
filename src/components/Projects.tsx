23ce530537d754212a38a66627446a8c6ca238dd
Gustavo Victor <gguvictor909@gmail.com>
2024-07-06 15:57:45 -0300

    feat: add projects animation

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,23 +1,38 @@
 import { PROJECTS } from "../data/info";
+import { motion } from "framer-motion";
 
 
 export default function Projects() {
     return (
         <section id="projects" className="border-b border-neutral-900 pb-4">
-            <h2 className="my-20 text-center text-4xl">Projetos</h2>
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="my-20 text-center text-4xl">
+                Projetos
+            </motion.h2>
             <div>
                 {PROJECTS.map((project) => (
-                    <div 
+                    <div
                         key={project.title}
                         className="mb-8 flex flex-wrap lg:justify-center">
-                        <div className="w-full lg:w-1/4">
-                            <img        
-                                height={192}                                                         
-                                src={project.image} 
+                        <motion.div 
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full lg:w-1/4">
+                            <img
+                                height={192}
+                                src={project.image}
                                 alt={project.title}
                                 className="mb-6 rounded w-full sm:max-w-48" />
-                        </div>
-                        <div className="w-full max-w-xl lg:w-3/4">
+                        </motion.div>
+                        <motion.div 
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full max-w-xl lg:w-3/4">
                             <h6 className="mb-2 font-semibold">{project.title}</h6>
                             <p className="mb-4 text-neutral-400">{project.description}</p>
                             <div className="flex gap-2 items-center flex-wrap">
@@ -25,7 +40,7 @@ export default function Projects() {
                                     <span className="rounded px-2 py-1 bg-neutral-900 text-purple-400 text-sm font-medium" key={techIndex}>{tech}</span>
                                 ))}
                             </div>
-                        </div>
+                        </motion.div>
                     </div>
                 ))}
             </div>
